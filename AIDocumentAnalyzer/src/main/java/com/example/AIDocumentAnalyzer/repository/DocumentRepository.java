cc30f9163410c0f3845022690974aa028a5b52eb
Marka Surendhar <marka.surendhar@zapcg.com>
2026-03-16 10:04:14 +0530

    Delete AIDocumentAnalyzer directory

--- AIDocumentAnalyzer/src/main/java/com/example/AIDocumentAnalyzer/repository/DocumentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.AIDocumentAnalyzer.repository;
-
-import com.example.AIDocumentAnalyzer.entity.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<Document,Long> {
-
-}
